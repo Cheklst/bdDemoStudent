268aaf7921277e53ad2ae4de9f8af8758d24c3aa
Cheklst <70970649+Cheklst@users.noreply.github.com>
2021-10-28 13:01:44 +0300

    Добавьте файлы проекта.

---
+++ bdDemoStudent/Class/Helper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using bdDemoStudent.View;
+using System.Diagnostics;
+
+namespace bdDemoStudent.Class
+{
+    public static class Helper
+    {
+        public static demo_wpfEntities demoBd; //Ссылка на БД
+        public static Activites activites;  //Ссылка активность
+        public static Users users;  //Ссылка на юзера
+        public static string PatchUser = "UserAvatar\\";  //Ссылка на аватары пользователей
+        public static string PatchJpg = "UserJpg\\";   //Ссылка на иконки
+
+        public static string PatchApplication(string str) =>
+            System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + "\\" + str;
+
+        static Helper()
+        {
+            try
+            {
+                demoBd = new demo_wpfEntities();
+            }
+            catch
+            {
+                View.MessageBox.Show("Ошибка при открытии БД", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error); 
+            }
+        }
+        //Шифрование(опционально)
+        public static string Md5Hash(string password)
+        {
+            return BitConverter.ToString(MD5.Create()
+                .ComputeHash(Encoding.UTF8.GetBytes(password)))
+                .Replace("-", "")
+                .ToLower();
+        }
+        //Запись в коллекцию
+        public static void ActiveAdd()
+        {
+            if (users == null) return;
+
+            if (activites == null)
+            {
+                activites = new Activites()
+                {
+                    IdUser = users.Id,
+                    LoginTime = DateTime.Now,
+                    Comment = "Успешный вход"
+                };
+
+                activites = demoBd.Activites.Add(activites);
+            }
+            else
+            {
+                activites.LogoutTime = DateTime.Now;
+                activites.Comment += "/Нормальный выход";
+            }
+            demoBd.SaveChanges();
+        }
+    }
+}
---
+++ bdDemoStudent/View/WinMain.xaml.cs
@@ -0,0 +1,50 @@
+﻿using bdDemoStudent.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bdDemoStudent.View
+{
+    public partial class WinMain : Window
+    {
+        public enum pageDemo
+        { 
+            PageAdmin = 1,
+            PageUser
+        }
+        public WinMain()
+        {
+            InitializeComponent();
+            WinLogin winLogin = new WinLogin();
+            winLogin.ShowDialog();
+            try
+            {
+                switch (Helper.users.Roles.Id)
+                {
+                    case (int)pageDemo.PageAdmin:
+                        MessageBox.Show("Администратор", "Выбор",
+                            MessageBoxButton.OK, MessageBoxImage.Warning);
+                        break;
+                    case (int)pageDemo.PageUser:
+                        MessageBox.Show("Пользователь", "Выбор", MessageBoxButton.OK,
+                            MessageBoxImage.Warning);
+                        break;
+                }
+            }
+            catch
+            {
+                Close();
+            }
+        }
+    }
+}
